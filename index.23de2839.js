a5ad922a3c2116b7b4e49e5bfab6bed1b4db2081
Dris11 <121932537+Dris11@users.noreply.github.com>
2023-04-07 16:10:44 +0000

    Deploying to gh-pages from @ Dris11/Project-Ice-Cream@f06f7d8b146e8ade0be6f3f0b38e73471f647b67 🚀

---
+++ index.23de2839.js
@@ -0,0 +1,2 @@
+window.addEventListener("DOMContentLoaded",(function(){var e=document.querySelectorAll(".customer-wrapper__header"),n=document.querySelector(".prev"),t=document.querySelector(".next"),c=1;function o(n){n>e.length&&(c=1),n<1&&(c=e.length),e.forEach((function(e){e.style.display="none"})),e[c-1].style.display="block"}function r(e){o(c+=e)}o(),n.addEventListener("click",(function(){r(-1)})),t.addEventListener("click",(function(){r(1)}))}));
+//# sourceMappingURL=index.23de2839.js.map
---
+++ index.7c47474a.js
@@ -0,0 +1,2 @@
+window.addEventListener("DOMContentLoaded",(()=>{const e=document.querySelectorAll(".customer-wrapper__header"),t=document.querySelector(".prev"),n=document.querySelector(".next");let c=1;function o(t){t>e.length&&(c=1),t<1&&(c=e.length),e.forEach((e=>{e.style.display="none"})),e[c-1].style.display="block"}function d(e){o(c+=e)}o(),t.addEventListener("click",(()=>{d(-1)})),n.addEventListener("click",(()=>{d(1)}))}));
+//# sourceMappingURL=index.7c47474a.js.map
