f06f7d8b146e8ade0be6f3f0b38e73471f647b67
Dris11 <121932537+Dris11@users.noreply.github.com>
2023-04-07 19:10:15 +0300

    00

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const slides = document.querySelectorAll('.customer-wrapper__header'),
+    prev = document.querySelector('.prev'),
+    next = document.querySelector('.next');
 
+  let slideIndex = 1;
+
+  showSlides();
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach(item => {
+      item.style.display = 'none';
+    });
+
+    slides[slideIndex - 1].style.display = 'block';
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  prev.addEventListener('click', () => {
+    plusSlides(-1);
+  });
+
+  next.addEventListener('click', () => {
+    plusSlides(1);
+  });
+});
